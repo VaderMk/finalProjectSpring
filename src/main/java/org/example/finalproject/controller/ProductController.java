763a4d3d0274e6aa6db7a7e3158a2b7d7ec7a99b
stefan <stefanmihairahlitchi@gmail.com>
2024-03-28 21:12:44 +0200

    cors

--- src/main/java/org/example/finalproject/controller/ProductController.java
+++ src/main/java/org/example/finalproject/controller/ProductController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("products")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
     @Autowired
     private ProductService productService;
